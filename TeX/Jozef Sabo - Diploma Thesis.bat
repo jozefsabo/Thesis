b9fea503f02e6f402856c2dd8851cb14bc228aed
Jozef Sabo <Jozef Sabo>
2012-04-13 04:40:02 +0200

    Renamed Master's Thesis to Diploma Thesis.

---
+++ TeX/Jozef Sabo - Diploma Thesis.bat
@@ -0,0 +1,14 @@
+"D:\Program Files\miktex\miktex\bin\pdflatex.exe" "Jozef Sabo - Diploma Thesis.tex"
+"D:\Program Files\miktex\miktex\bin\pdflatex.exe" "Jozef Sabo - Diploma Thesis.tex"
+
+del "Jozef Sabo - Diploma Thesis.aux"
+del "Jozef Sabo - Diploma Thesis.log"
+del "Jozef Sabo - Diploma Thesis.lot"
+del "Jozef Sabo - Diploma Thesis.loc"
+del "Jozef Sabo - Diploma Thesis.lof"
+del "Jozef Sabo - Diploma Thesis.toc"
+del "Jozef Sabo - Diploma Thesis.out"
+
+"Jozef Sabo - Diploma Thesis.pdf"
+
+exit
